2d4afa834cc3a078bd3fd0311186278d8cb040f0
Kenneth Fisher <kenandcrys@live.com>
2023-10-05 13:32:05 -0500

    this is really the last one

--- backend/routes/api/cattle.js
+++ backend/routes/api/cattle.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { Cattles } = require("../../db/models");
+const { Cattle } = require("../../db/models");
 
 // ROUTE TO GET ALL OF THE Cattle
 router.get("/", async (_req, res, next) => {
   try {
-    const cattle = await Cattles.findAll();
-    console.log(cattle);
+    const cattle = await Cattle.findAll();
+    
     res.json(cattle);
   } catch (error) {
     next(error);
