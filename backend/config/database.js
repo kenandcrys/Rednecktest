c621230be9feaf0c86c9fe9de206331f71c92f55
Kenneth Fisher <kenandcrys@live.com>
2023-10-04 22:49:18 -0500

    another test

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,7 +10,7 @@ module.exports = {
     typeValidation: true
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+   
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
