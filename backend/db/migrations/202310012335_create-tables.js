cf3b845e4d9406730d7deaa3a7c498890e11ba58
Kenneth Fisher <kenandcrys@live.com>
2023-10-07 12:33:44 -0500

    here

--- backend/db/migrations/202310012335_create-tables.js
+++ backend/db/migrations/202310012335_create-tables.js
@@ -52,31 +52,6 @@ module.exports = {
       },
     }, options);
 
-    // Insert data into the table
-    await queryInterface.bulkInsert(tableName, [
-      {
-        name: "16% Dairy",
-        description: "16% protein dairy cow feed",
-        pricePerBag: 20.0,
-        pricePer20: 19.0,
-        pricePer40: 18.5,
-        url: "https://i.imgur.com/sZwrxu6.png",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "BYB",
-        description: "Barn yard buffet (all stock feed)",
-        pricePerBag: 18.25,
-        pricePer20: 17.25,
-        pricePer40: 16.75,
-        url: "https://i.imgur.com/g7Bt1a7.png",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      // Add more data rows as needed...
-    ], {});
-
     // Return a Promise to ensure the migration is complete
     return Promise.resolve();
   },
