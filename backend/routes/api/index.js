f750543f1cf120e0eac7cabd5b0da8527c7532a7
Kenneth Fisher <kenandcrys@live.com>
2023-10-07 21:39:36 -0500

    hopin for the best

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,6 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
-const sessionRouter = require("./session.js");
+
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -12,9 +12,5 @@ const CattleRouter = require("./Cattle.js");
 // Register your API routes
 router.use("/cattle", CattleRouter);
 //app.use(routes);
-
-router.post("/test", (req, res) => {
-  res.json({ requestBody: req.body });
-});
 
 module.exports = router;
